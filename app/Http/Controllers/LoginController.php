989d37f923b691a333df792546a2c0a297c5fc6f
Weydans Barros <weydans@hotmail.com>
2023-05-02 14:23:33 -0300

    feat: create login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginFormRequest;
+use App\Http\Resources\LoginResource;
+
+class LoginController extends Controller
+{
+	public function login(LoginFormRequest $request)
+	{
+		$requestData = $request->only(['email', 'password']);
+
+		$isLogged = Auth::attempt( $requestData );
+
+		if ( !$isLogged ) {
+			return response()->json(["message" => 'E-mail e/ou senha incorretos'], 401);
+		}
+		
+		/** @var User */
+		$user  = Auth::user();
+		$token = $user->createToken('AccessToken');
+
+		return LoginResource::make([
+			'user'           => $user,
+			'accessToken'    => $token->accessToken,
+			'plainTextToken' => $token->plainTextToken,
+		]);
+	}
+}
